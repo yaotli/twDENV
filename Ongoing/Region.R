964a93ff59c87c404d5b725eb4974707d6f200d5
Yao Li <nprotei@gmail.com>
2016-07-08 18:41:06 +0800

    add region.R

---
+++ Ongoing/Region.R
@@ -0,0 +1,9 @@
+#this .R file focus on region recognition
+
+#lsfullm = lsfull[-(pairedS.No[65])]
+#lsfull.C0m = lsfull.C0[-103]
+#pairedS.Nom
+
+
+breaks = seq(930, 2430, by=5) # length(breaks) = 301
+
